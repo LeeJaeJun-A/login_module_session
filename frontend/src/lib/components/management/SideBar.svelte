50b3f98c17f0f0cd04925c62cda4be74c04c93d5
LeeJaeJun-A <sjaqjnjs@naver.com>
2024-09-23 13:55:58 +0900

    Feat: Complete sidebar structure

--- frontend/src/lib/components/management/SideBar.svelte
+++ frontend/src/lib/components/management/SideBar.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { logout } from "$lib/components/login/login";
-  export let userid: string;
-
-  onMount(() => {});
+  import { getUserId, logout } from "$lib/components/login/login";
 </script>
 
 <aside
@@ -13,13 +9,19 @@
 >
   <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
     <ul class="space-y-2 font-medium">
-      <li>Hello</li>
+      <li>
+        <p
+          class="flex items-center w-full p-2 whitespace-nowrap border-b border-gray-300"
+        >
+          Hello {getUserId()}!
+        </p>
+      </li>
       <li>
         <button
           class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group w-full"
         >
           <svg
-            class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+            class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white flex"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -42,13 +44,16 @@
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
-            viewBox="0 0 20 20"
+            viewBox="4 3 20 20"
           >
             <path
-              d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"
+              fill-rule="evenodd"
+              d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z"
+              clip-rule="evenodd"
             />
           </svg>
-          <span class="flex-1 ms-3 whitespace-nowrap text-left">Inbox</span>
+          <span class="flex-1 ms-3 whitespace-nowrap text-left">Lock Users</span
+          >
           <span
             class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300"
             >3</span
@@ -61,11 +66,11 @@
           on:click={logout}
         >
           <svg
-            class="flex-shrink-0 w-5 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+            class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
-            viewBox="0 0 20 20"
+            viewBox="0 2 22 20"
           >
             <path
               stroke="currentColor"
